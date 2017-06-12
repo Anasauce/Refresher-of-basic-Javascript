7fe3cf45c5d22e97235ff1d68e679dab08628cc4
anasauce <anasauceda1843@gmail.com>
2017-06-11 18:22:46 -0700

    do while loop exercise

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//basic number guessing game with 'do while' loop
+
+var randomNumber = getRandomNumber(10);
+var guess;
+var guessCount = 0;
+var correctGuess = false;
+
+function getRandomNumber( upper ) {
+  var num = Math.floor(Math.random() * upper) + 1;
+  return num;
+}
+
+do {
+  guess = prompt('I am thinking of a number between 1 and 10');
+  guessCount += 1;
+  if (parseInt(guess) === randomNumber ) {
+    correctGuess = true;
+  }
+} while (! correctGuess)
+  document.write('<h1>You guessed the number!</h1>');
+  document.write('It took you ' + guessCount + ' guesses to get ' + randomNumber );
